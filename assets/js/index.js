2b6c219538323105c2adc1d5cce87005b33c69f1
Scotty Phillips <scotty.d.phillips@gmail.com>
2022-08-22 15:03:49 -0400

    styling, js filtering by day

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,25 +8,93 @@ const sunLink = document.getElementById('sun-link');
 const sunSchedule = document.getElementById('sun-schedule');
 
 showThursSchedule = () => {
-  if (!thursSchedule.classList.contains('showing')) {
-    thursSchedule.classList.add('showing');
-    if (!friSchedule.classList.contains('collapse')) {
-      friSchedule.classList.add('collapse');
+  if (!thursSchedule.hasAttribute('active')) {
+    thursSchedule.setAttribute('style', 'display: block;');
+    thursSchedule.setAttribute('active', 'active');
+    if (friSchedule.hasAttribute('active')) {
+      friSchedule.removeAttribute('active');
     }
-    if (!satSchedule.classList.contains('collapse')) {
-      satSchedule.classList.add('collapse');
+    friSchedule.setAttribute('style', 'display: none;');
+    if (satSchedule.hasAttribute('active')) {
+      satSchedule.removeAttribute('active');
     }
-    if (!sunSchedule.classList.contains('collapse')) {
-      sunSchedule.classList.add('collapse');
+    satSchedule.setAttribute('style', 'display: none;');
+    if (sunSchedule.hasAttribute('active')) {
+      sunSchedule.removeAttribute('active');
     }
+    sunSchedule.setAttribute('style', 'display: none;');
   }
 }
 
 thursLink.addEventListener('click', () => {
   showThursSchedule();
-})
+});
 
 showFriSchedule = () => {
-  thursSchedule.classList.add('hidden');
-
+  if (!friSchedule.hasAttribute('active')) {
+    if (thursSchedule.hasAttribute('active')) {
+      thursSchedule.removeAttribute('active');
+    }
+    thursSchedule.setAttribute('style', 'display: none;');
+    friSchedule.setAttribute('active', 'active');
+    friSchedule.setAttribute('style', 'display: block;');
+    if (satSchedule.hasAttribute('active')) {
+      satSchedule.removeAttribute('active');
+    }
+    satSchedule.setAttribute('style', 'display: none;')
+    if (sunSchedule.hasAttribute('active')) {
+      sunSchedule.removeAttribute('active');
+    }
+    sunSchedule.setAttribute('style', 'display: none;');
+  }
 }
+
+friLink.addEventListener('click', () => {
+  showFriSchedule();
+});
+
+showSatSchedule = () => {
+  if (!satSchedule.hasAttribute('active')) {
+    if (thursSchedule.hasAttribute('active')) {
+      thursSchedule.removeAttribute('active');
+    }
+    thursSchedule.setAttribute('style', 'display: none;');
+    if (friSchedule.hasAttribute('active')) {
+      friSchedule.removeAttribute('active');
+    }
+    friSchedule.setAttribute('style', 'display: none;');
+    satSchedule.setAttribute('active', 'active');
+    satSchedule.setAttribute('style', 'display: block;');
+    if (sunSchedule.hasAttribute('active')) {
+      sunSchedule.removeAttribute('active');
+    }
+    sunSchedule.setAttribute('style', 'display: none;');
+  }
+}
+
+satLink.addEventListener('click', () => {
+  showSatSchedule();
+});
+
+showSunSchedule = () => {
+  if (!sunSchedule.hasAttribute('active')) {
+    if (thursSchedule.hasAttribute('active')) {
+      thursSchedule.removeAttribute('active');
+    }
+    thursSchedule.setAttribute('style', 'display: none');
+    if (friSchedule.hasAttribute('active')) {
+      friSchedule.removeAttribute('active');
+    }
+    friSchedule.setAttribute('style', 'display: none;');
+    if (satSchedule.hasAttribute('active')) {
+      satSchedule.removeAttribute('active')
+    }
+    satSchedule.setAttribute('style', 'display: none;');
+    sunSchedule.setAttribute('active', 'active');
+    sunSchedule.setAttribute('style', 'display: block;');
+  }
+}
+
+sunLink.addEventListener('click', () => {
+  showSunSchedule();
+});
